0b502dbc7fa81427e8e8abf6d7f746172e25bf66
lovanshgaur <lovanshgaur@gmail.com>
2025-11-27 10:32:31 +0530

    TODO Logic Done

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,12 +1,37 @@
+import { useState } from "react";
 import ToDoItem from "./ToDoItem";
 
-const tasks = ["task 1", "task 2", "task 3", "task 4", "task 5"];
 const ToDoList = () => {
+  const [tasks, setTasks] = useState([
+    "task 1",
+    "task 2",
+    "task 3",
+    "task 4",
+    "task 5"
+  ]);
+
   function handleDone(name) {
-    console.log("Done", name);
+    const newTasks = tasks.filter((task) => task !== name)
+    setTasks(newTasks)
   }
+
+  function addTask(name) {
+    if (name) {
+      console.log("Added", name);
+      const newTasks = [...tasks, name];
+      setTasks(newTasks)
+      setInput('')
+    } else {
+      console.error("No task Added");
+    }
+  }
+
+  const [input, setInput] = useState("");
   return (
     <>
+      <p>{input}</p>
+      <input type="text" onChange={(e) => setInput(e.target.value)} />
+      <button onClick={() => addTask(input)}>Add Task</button>
       {tasks.map((task, index) => (
         <ToDoItem key={index} task={task} onDone={handleDone} />
       ))}
